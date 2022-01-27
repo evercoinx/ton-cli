aa9f0c55ccf61297a561e142fa99e4b2e1d3a848
Serge Grigorenko <serge@evercloud.io>
2022-01-27 22:14:05 +0200

    Implement predeploy and deploy commands

--- bin/main.js
+++ bin/main.js
@@ -10,10 +10,26 @@ const provider = new HttpProvider(process.env.HTTP_PROVIDER_HOST);
 const tonweb = new TonWeb(provider);
 tonweb.wallet.defaultVersion = "v3R2";
 
-const wallet = new Wallet(tonweb);
+const wallet = new Wallet(tonweb, 0);
 
 (async () => {
   yargs(process.argv.slice(2))
+    .command({
+      command: "walletpredeploy",
+      aliases: ["wp"],
+      desc: "Predeploy wallet",
+      handler: async () => {
+        await wallet.predeploy();
+      },
+    })
+    .command({
+      command: "walletdeploy <address>",
+      aliases: ["wd"],
+      desc: "Deploy wallet",
+      handler: async (argv) => {
+        await wallet.deploy(argv.address);
+      },
+    })
     .command({
       command: "walletinfo <address>",
       aliases: ["wi"],
--- internal/wallet.js
+++ internal/wallet.js
@@ -1,97 +1,151 @@
-const mnemonic = require("../mnemonic.json");
+const fs = require("fs").promises;
 const tonMnemonic = require("tonweb-mnemonic");
 const { BN } = require("tonweb").utils;
+const mnemonicData = require("../mnemonic.json");
 
 class Wallet {
-  constructor(tonweb) {
+  constructor(tonweb, workchain) {
     this.tonweb = tonweb;
+    this.workchain = workchain;
+  }
+
+  async predeploy() {
+    try {
+      const walletMnemonic = await tonMnemonic.generateMnemonic();
+      const keyPair = await tonMnemonic.mnemonicToKeyPair(walletMnemonic);
+
+      const wallet = this.tonweb.wallet.create({
+        publicKey: keyPair.publicKey,
+        wc: this.workchain,
+      });
+
+      const address = await wallet.getAddress();
+      const bouncableAddress = address.toString(true, true, true);
+      mnemonicData[bouncableAddress] = walletMnemonic;
+      await fs.writeFile(
+        "mnemonic.json",
+        JSON.stringify(mnemonicData, null, 4)
+      );
+
+      const deployRequest = await wallet.deploy(keyPair.secretKey);
+
+      const feeResponse = await deployRequest.estimateFee();
+      this.printFees(feeResponse);
+
+      const nonBouncableAddress = address.toString(true, true, false);
+      console.log(`Wallet is ready to be deployed at ${nonBouncableAddress}`);
+    } catch (err) {
+      console.error(`Error! ${err}`);
+    }
   }
 
   async deploy(walletAddress) {
-    const walletMnemonic = mnemonic[walletAddress];
-    if (!walletMnemonic) {
-      console.error(`Error! Wallet mnemonic is not found`);
-      return;
+    try {
+      const walletMnemonic = mnemonicData[walletAddress];
+      if (!walletMnemonic) {
+        console.error(`Error! Wallet mnemonic is not found`);
+        return;
+      }
+
+      const validMnemonic = await tonMnemonic.validateMnemonic(walletMnemonic);
+      if (!validMnemonic) {
+        console.error(`Error! Mnemonic is invalid`);
+        return;
+      }
+
+      const keyPair = await tonMnemonic.mnemonicToKeyPair(walletMnemonic);
+
+      const wallet = this.tonweb.wallet.create({
+        publicKey: keyPair.publicKey,
+        wc: this.workchain,
+      });
+
+      const deployRequest = await wallet.deploy(keyPair.secretKey);
+
+      const feeResponse = await deployRequest.estimateFee();
+      this.printFees(feeResponse);
+
+      const response = await deployRequest.send();
+      this.printResponse(response, `Wallet was deployed successfully`);
+    } catch (err) {
+      console.error(`Error! ${err}`);
     }
-
-    const keyPair = await tonMnemonic.mnemonicToKeyPair(senderMnemonic);
-
-    const wallet = this.tonweb.wallet.create({
-      publicKey: keyPair.publicKey,
-      wc: 0,
-    });
-
-    const deployRequest = await wallet.deploy(keyPair.secretKey);
-
-    const feeResponse = await deployRequest.estimateFee();
-    this.printFees(feeResponse);
-
-    const response = await deployRequest.send();
-    this.printResponse(response, `Wallet was deployed successfully`);
-
-    await this.info(walletAddress);
   }
 
   async info(walletAddress) {
-    const wallet = this.tonweb.wallet.create({ address: walletAddress });
+    try {
+      const wallet = this.tonweb.wallet.create({ address: walletAddress });
 
-    const address = await wallet.getAddress();
-    const seqno = await wallet.methods.seqno().call();
-    const balance = await this.tonweb.getBalance(walletAddress);
+      const address = await wallet.getAddress();
+      const seqno = await wallet.methods.seqno().call();
+      const balance = await this.tonweb.getBalance(walletAddress);
 
-    console.log("\nWallet information:");
-    console.log(`- Raw address: ${address.toString(false, true, true)}`);
-    console.log(`- Bouncable address: ${address.toString(true, true, true)}`);
-    console.log(
-      `- Non bouncable address: ${address.toString(true, true, false)}`
-    );
-    console.log(`- Balance: ${this.formatAmount(balance)}`);
-    console.log(`- Sequence number: ${seqno}`);
+      console.log("\nWallet information:");
+      console.log(`- Raw address: ${address.toString(false, true, true)}`);
+      console.log(`- Bouncable address: ${address.toString(true, true, true)}`);
+      console.log(
+        `- Non bouncable address: ${address.toString(true, true, false)}`
+      );
+      console.log(`- Balance: ${this.formatAmount(balance)}`);
+      console.log(`- Sequence number: ${seqno}`);
+    } catch (err) {
+      console.error(`Error! ${err}`);
+    }
   }
 
   async transfer(sender, recipient, amount) {
-    const senderMnemonic = mnemonic[sender];
-    if (!senderMnemonic) {
-      console.error(`Error! Sender mnemonic is not found`);
-      return;
-    }
+    try {
+      const senderMnemonic = mnemonicData[sender];
+      if (!senderMnemonic) {
+        console.error(`Error! Sender mnemonic is not found`);
+        return;
+      }
 
-    const keyPair = await tonMnemonic.mnemonicToKeyPair(senderMnemonic);
+      const validMnemonic = await tonMnemonic.validateMnemonic(senderMnemonic);
+      if (!validMnemonic) {
+        console.error(`Error! Mnemonic is invalid`);
+        return;
+      }
 
-    const wallet = this.tonweb.wallet.create({
-      publicKey: keyPair.publicKey,
-      wc: 0,
-    });
+      const keyPair = await tonMnemonic.mnemonicToKeyPair(senderMnemonic);
 
-    const amountNano = this.tonweb.utils.toNano(amount);
-    const senderBalance = await this.tonweb.getBalance(sender);
-    if (amountNano.gt(new BN(senderBalance))) {
-      console.error(
-        `Error! Transfer amount ${this.formatAmount(
-          amountNano
-        )} exceeds balance ${this.formatAmount(senderBalance)}`
+      const wallet = this.tonweb.wallet.create({
+        publicKey: keyPair.publicKey,
+        wc: this.workchain,
+      });
+
+      const amountNano = this.tonweb.utils.toNano(amount);
+      const senderBalance = await this.tonweb.getBalance(sender);
+      if (amountNano.gt(new BN(senderBalance))) {
+        console.error(
+          `Error! Transfer amount ${this.formatAmount(
+            amountNano
+          )} exceeds balance ${this.formatAmount(senderBalance)}`
+        );
+        return;
+      }
+
+      const seqno = await wallet.methods.seqno().call();
+      const transferRequest = wallet.methods.transfer({
+        secretKey: keyPair.secretKey,
+        toAddress: recipient,
+        amount: amountNano,
+        seqno: seqno,
+        payload: "Transfer",
+        sendMode: 3,
+      });
+
+      const feeResponse = await transferRequest.estimateFee();
+      this.printFees(feeResponse);
+
+      const transferResponse = await transferRequest.send();
+      this.printResponse(
+        transferResponse,
+        `${this.formatAmount(amountNano)} were transferred successfully`
       );
-      return;
+    } catch (err) {
+      console.error(`Error! ${err}`);
     }
-
-    const seqno = await wallet.methods.seqno().call();
-    const transferRequest = wallet.methods.transfer({
-      secretKey: keyPair.secretKey,
-      toAddress: recipient,
-      amount: amountNano,
-      seqno: seqno,
-      payload: "Transfer",
-      sendMode: 3,
-    });
-
-    const feeResponse = await transferRequest.estimateFee();
-    this.printFees(feeResponse);
-
-    const transferResponse = await transferRequest.send();
-    this.printResponse(
-      transferResponse,
-      `${this.formatAmount(amountNano)} were transferred successfully`
-    );
   }
 
   formatAmount(amount) {
@@ -108,9 +162,10 @@ class Wallet {
 
     const fees = response["source_fees"];
     console.log(`Fees:`);
-    console.log(`- Forward fee: ${this.formatAmount(fees["fwd_fee"])}`);
-    console.log(`- Gas fee:     ${this.formatAmount(fees["gas_fee"])}`);
-    console.log(`- Storage fee: ${this.formatAmount(fees["storage_fee"])}`);
+    console.log(`- Gas fee:        ${this.formatAmount(fees["gas_fee"])}`);
+    console.log(`- In-Forward fee: ${this.formatAmount(fees["in_fwd_fee"])}`);
+    console.log(`- Forward fee:    ${this.formatAmount(fees["fwd_fee"])}`);
+    console.log(`- Storage fee:    ${this.formatAmount(fees["storage_fee"])}`);
   }
 
   printResponse(response, successMessage) {
